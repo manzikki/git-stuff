f47d457f07f8c65a356a62d8767e82ea036ff461
Xin Zhou <xin.zhou@unige.ch>
2010-10-15 21:39:49 +0200

    - Add comments in the code     - clean the code

--- modules/bibrank/lib/bibrank_gift_indexer.py
+++ modules/bibrank/lib/bibrank_gift_indexer.py
@@ -19,12 +19,16 @@
 ## You should have received a copy of the GNU General Public License
 ## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-__revision__ = "$Id: bibrank_gift_indexer.py,v 1.93 2009/12/10 10:59:01 Xin Exp $"
+__revision__ = "$Id: bibrank_gift_indexer.py, 2009/12/10 10:59:01 Xin Exp $"
 
-import time, csv, re, os, os.path, shutil, urllib, urllib2, webbrowser, xml.dom
+import os, os.path, shutil, urllib, urllib2, xml.dom, sys
+import time, csv, re, warnings
 import distutils.dir_util as DU
 import xml.etree.ElementTree as ET
-import ConfigParser
+import ConfigParser 
+
+if sys.hexversion < 0x2040000:
+    from sets import Set
 
 from invenio.bibformat_engine import *
 from invenio.shellutils import *
@@ -34,23 +38,48 @@ from invenio.bibtask import task_get_option
 from invenio.dbquery import run_sql
 from invenio.errorlib import register_exception
 from invenio.bibtask import write_message
-from invenio.config import CFG_PREFIX, CFG_TMPDIR, CFG_PATH_WGET, CFG_CERN_SITE, CFG_ETCDIR
+from invenio.config import CFG_PREFIX, CFG_TMPDIR, CFG_CERN_SITE, CFG_ETCDIR
 
-# variables :
+""" below are the location variables for the gnuift config files.
+    they are under the cds-invenio/var folder
+    two folders are required to be created for gnuift :
+    gift_config and gift-index-data :
+    gift_config contains config file for each collection
+    (by default we considered the cds-invenio as one collection)
+    gift-index-data contains :
+        - feature data extracted from each image (*.fts)
+        - mapping file from image url to feature file (url2fts.xml)
+        - 3 index files for query : 
+            - InvertedFile.db, 
+            - InvertedFileOffset.db,
+            - InvertedFileFeatureDescription.db
+"""
 CFG_GIFTINDEX_PREFIX = CFG_PREFIX + "/var/gift-index-data"
-CFG_PATH_URL2FTS = CFG_GIFTINDEX_PREFIX+"/url2fts.xml"
-CFG_GIFTINDEX_DESC = CFG_GIFTINDEX_PREFIX + "/InvertedFileFeatureDescription.db"
+CFG_PATH_URL2FTS  = CFG_GIFTINDEX_PREFIX+"/url2fts.xml"
+CFG_GIFTINDEX_DESC= CFG_GIFTINDEX_PREFIX + "/InvertedFileFeatureDescription.db"
 CFG_GIFTCONFIG_PREFIX = CFG_PREFIX + "/var/gift_config"
 CFG_PATH_GIFTCONFIG = CFG_GIFTCONFIG_PREFIX+"/gift-config.mrml"
-CFG_PATH_GIFTCONFIG_TEMPLATE = "/usr/share/libmrml1/gift-config.mrml"
 
+""" below are the location variables for the gnuift executables.
+    they depends on where gnuift is installed
+    - convert is part of ImageMagick executables, it is used
+      to change images into standard ppm format.
+    - gift-extract-features is to extract features from one image
+    - gift-write-feature-descs is to write down what kind of 
+      features are in use.
+    - gift-generate-inverted-file generate index file from 
+      feature files.
+    To extract features from files, some temporary files 
+    are needed
+""" 
+CFG_PATH_CONVERT = "/usr/bin/convert"
 CFG_PATH_GIFTFTSEXTRACT = "/usr/bin/gift-extract-features"
 CFG_PATH_GIFTWRITEFTSDESC = "/usr/bin/gift-write-feature-descs"
 CFG_PATH_GIFTINDEXGENERATION = "/usr/bin/gift-generate-inverted-file"
-CFG_PATH_CONVERT = "/usr/bin/convert"
-tmp_ppm = CFG_TMPDIR+"/gift_tmp.ppm"
-tmp_desc= CFG_TMPDIR+"/gift_tmp.db"
-tmp_fts = CFG_TMPDIR+"/gift_tmp.fts"
+CFG_PATH_GIFTCONFIG_TEMPLATE = "/usr/share/libmrml1/gift-config.mrml"
+tmp_ppm = CFG_TMPDIR + "/gift_tmp.ppm"
+tmp_desc = CFG_TMPDIR + "/gift_tmp.db"
+tmp_fts = CFG_TMPDIR + "/gift_tmp.fts"
 
 ext2conttype = {"jpg": "image/jpeg",
                 "jpeg": "image/jpeg",
@@ -58,11 +87,15 @@ ext2conttype = {"jpg": "image/jpeg",
                 "gif": "image/gif"}
 imgurl2recid = {}
 
+""" MyHTTPRedirect is used to dealing with 
+    any redirection of the url. 
+"""
 redirect_handler = urllib2.HTTPRedirectHandler()
 class MyHTTPRedirectHandler(urllib2.HTTPRedirectHandler):
     def http_error_302(self, req, fp, code, msg, headers):
         print "Cookie Manip Right Here"
-        return urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
+        return urllib2.HTTPRedirectHandler.http_error_302(
+               self, req, fp, code, msg, headers)
     http_error_301 = http_error_303 = http_error_307 = http_error_302
 
 cookieprocessor = urllib2.HTTPCookieProcessor()
@@ -70,10 +103,13 @@ cookieprocessor = urllib2.HTTPCookieProcessor()
 opener = urllib2.build_opener(MyHTTPRedirectHandler, cookieprocessor)
 urllib2.install_opener(opener)
 
-#FIXME: add gift server stop and start
-#               make the port used for gift parametrable
 def gnuift_similarity(rank_method_code):
-    """Rank by the Gnu Image Finding Tools."""
+    """Rank records by the Gnu Image Finding Tools.
+       It is the main entry of the whole file.
+       It is called automatically by bibrank.py
+    """
+    
+    """ Read bibrank config file"""
     config = cfgGetParameters(rank_method_code)
     icon_obj = config.get("gnuift_similarity", "icon_obj")
     icon_label_tag = config.get("gnuift_similarity", "icon_label_tag")
@@ -83,47 +119,75 @@ def gnuift_similarity(rank_method_code):
     recids = []
     imgurl2recid = {}
     op = task_get_option("quick")
-    os.system("killall -9 gift")
+    """ kill all running gift processes"""
+    os.system("killall -9 gift") 
 
-    write_message("Image urls are found at : %s", icon_url_tag)
-    write_message("Redo all feature extraction?? : %s", op)
-    write_message("Gnuift features are stored in : %s", CFG_GIFTINDEX_PREFIX)
-    write_message("Gnuift config files are stored in: %s", CFG_GIFTCONFIG_PREFIX)
+    write_message("Image urls are found at : %s" % 
+        (icon_obj+"_"+icon_url_tag), stream=sys.stdout, verbose=3)
+    write_message("Redo all feature extraction?? : %s" %
+        op, stream=sys.stdout, verbose=3)
+    write_message("Gnuift features are stored in : %s" %
+        CFG_GIFTINDEX_PREFIX, stream=sys.stdout, verbose=3)
+    write_message("Gnuift config files are stored in: %s" %
+        CFG_GIFTCONFIG_PREFIX, stream=sys.stdout, verbose=3)
 
+    """ using perform_requrest_research to search all the records
+        that contains the preseted value in label tag
+    """
     icon_p = icon_obj+icon_label_tag+':'+icon_label_val
     recids = perform_request_search(p=icon_p)
 
-    if op and op=="no":
+    """ if bibrank option is -R, all features need to be rebuilt
+        remove all extracted features
+    """
+    if op and op == "no":
         if os.path.isdir(CFG_GIFTINDEX_PREFIX):
             DU.remove_tree(CFG_GIFTINDEX_PREFIX)
     else:
         last_updated = get_bibrankmethod_lastupdate(rank_method_code)
-        write_message("Last time Gnuift extracted features was : %s", last_updated)
+        write_message("Last time Gnuift extracted features was : %s" %
+            last_updated, stream=sys.stdout, verbose=3)
         modified_recids = get_last_modified_rec(last_updated)
+        """ recids contains records that contains images.
+            modified_recids contains the modified records.
+            the intersection of these two collections are
+            modified records which contains images.
+        """
         recids = list(Set(recids) & Set(modified_recids))
 
-    imgurl2recid = add_icon_recid(recids, icon_obj, icon_label_tag, icon_label_val, icon_url_tag)
+    imgurl2recid = add_icon_recid(recids, icon_obj, icon_label_tag, 
+                                  icon_label_val, icon_url_tag)
 
-    write_message("The number of urls taken is : %s", len(imgurl2recid.keys()))
+    write_message("The number of urls taken is : %s" %
+        (len(imgurl2recid.keys())), stream=sys.stdout, verbose=3)
+
+    """ Create the two folders for config file and index files """
     DU.mkpath(CFG_GIFTINDEX_PREFIX)
     DU.mkpath(CFG_GIFTCONFIG_PREFIX)
 
+    """ Build element tree for url2fts.xml"""
     url2ftsET = ET.ElementTree()
     imagelist = ET.Element("image-list")
 
+    """ If url2fts.xml exists, read it into url2ftsET
+        else, build a new one.
+    """
     if os.path.isfile(CFG_PATH_URL2FTS):
         url2ftsET.parse(CFG_PATH_URL2FTS)
         imagelist = url2ftsET.find("image-list")
     else :
         url2ftsET = ET.ElementTree(imagelist)
 
+    """ Read all existing image urls into urllist """
     urllist = []
     xmlnodes = url2ftsET.findall("//image")
     for xmlnode in xmlnodes:
-        print xmlnode.attrib["url-postfix"]
+        existingurl = xmlnode.attrib["url-postfix"]
         urllist.append(xmlnode.attrib["url-postfix"])
 
-    print len(imgurl2recid.keys())
+    write_message("The number of urls taken is : %s" %
+        (len(imgurl2recid.keys())), stream=sys.stdout, verbose=3)
+
     if os.path.isfile(tmp_ppm):
         os.remove(tmp_ppm)
     if os.path.isfile(tmp_desc):
@@ -140,18 +204,22 @@ def gnuift_similarity(rank_method_code):
             except:
                 register_exception()
 
-    print CFG_PATH_URL2FTS+"----------"
+    """ write the xml tree into CFG_PATH_URL2FTS file """
     url2ftsET.write(CFG_PATH_URL2FTS)
     os.system(CFG_PATH_GIFTINDEXGENERATION+" "+CFG_GIFTINDEX_PREFIX+"/")
     # TODO : using run_shell_command???
-    #run_shell_command(CFG_PATH_GIFTINDEXGENERATION+" %s", (CFG_GIFTINDEX_PREFIX+"/",))
-    print "\nPROGRESS: 99%\n"
+    # run_shell_command(CFG_PATH_GIFTINDEXGENERATION+" %s", 
+    #                  (CFG_GIFTINDEX_PREFIX+"/",))
 
-    if op=="no" or not os.path.isfile(CFG_PATH_GIFTCONFIG):
+    """ only if option is -R or config file does not exist,
+        build a new gift config file
+    """
+    if op == "no" or not os.path.isfile(CFG_PATH_GIFTCONFIG):
         generate_gift_config_file(len(imgurl2recid.keys()))
 
     ndate = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    run_sql("UPDATE rnkMETHOD SET last_updated=%s WHERE name=%s",(ndate, rank_method_code))
+    run_sql("UPDATE rnkMETHOD SET last_updated=%s WHERE name=%s",
+            (ndate, rank_method_code))
     os.system("gift --port 12700 --datadir "+CFG_GIFTCONFIG_PREFIX+" &")
 
 def is_image(filename):
@@ -160,6 +228,10 @@ def is_image(filename):
     return filename[filename.rfind(".")+1:] in ext2conttype
 
 def add_url_recid(recids, fieldname):
+    """ Building a map of image url to recids. 
+        Fieldname indicate the field where image url can be found.
+        Deprecated !! Replaced by add_icon_recid
+    """
     tmp_dict = {}
     for recid in recids:
         urls = get_fieldvalues(recid, fieldname)
@@ -169,13 +241,20 @@ def add_url_recid(recids, fieldname):
                     ('documents.cern.ch' in url or \
                     'doc.cern.ch' in url or \
                     'preprints.cern.ch' in url):
-                    print 'dropped urls: %s' %url
+                    write_message("dropped urls: %s" %url, 
+                        stream=sys.stderr, verbose=0)    
                 else:
-                    tmp_dict[url]=recid
-    print len(tmp_dict.keys())
+                    tmp_dict[url] = recid
+    write_message("Number of valid url found: %s" %
+        (len(tmp_dict.keys())), stream=sys.stdout, verbose=3)
     return tmp_dict
 
-def add_icon_recid(recids, icon_obj, icon_label_tag, icon_label_val, icon_url_tag):
+def add_icon_recid(recids, icon_obj, icon_label_tag, 
+                   icon_label_val, icon_url_tag):
+    """ Building a map of image url to recids by the parameters 
+        defined in configuration file.
+        BibFormatObject is used to simplify the tasks.
+    """
     tmp_dict = {}
     icon_url = ""
     for recid in recids:
@@ -189,33 +268,55 @@ def add_icon_recid(recids, icon_obj, icon_label_tag, icon_label_val, icon_url_ta
                 if ('documents.cern.ch' in icon_url or \
                     'doc.cern.ch' in icon_url or \
                     'preprints.cern.ch' in icon_url):
-                    print 'dropped urls: %s' %icon_url
+                    write_message("dropped urls: %s" %icon_url, 
+                        stream=sys.stderr, verbose=0) 
                 else:
-                    tmp_dict[icon_url]=recid
+                    tmp_dict[icon_url] = recid
+    write_message("Number of valid url found: %s" %
+        (len(tmp_dict.keys())), stream=sys.stdout, verbose=3)
     return tmp_dict
 
 def extract_features(url):
-    print url+'\n'
+    """ This function download image by url.
+        Image is stored in a temporary file and features  
+        are extracted from that file.
+        When finished, temporary file will be deleted.
+    """
+    write_message("Feature extraction processing for %s " %
+        url, stream=sys.stdout, verbose=3) 
     urlsegs = url.split("/")
-    filename= urlsegs[len(urlsegs)-1]
-    print "filename: %s\n" %filename
-    tmp_img = CFG_TMPDIR+"/"+filename
+    filename = urlsegs[len(urlsegs)-1]
+    write_message("filename: %s\n" %filename %
+        url, stream=sys.stdout, verbose=3)
+    tmp_img = CFG_TMPDIR + "/" + filename
+
+    """Downloading images by url"""
     f_resp = urllib2.urlopen(url)
     urllib.urlretrieve(f_resp.geturl(),tmp_img)
     f_resp.close()
-    #os.system(CFG_PATH_WGET + " -P "+CFG_TMPDIR+" "+url)
-    os.system(CFG_PATH_CONVERT+" -geometry 256x256! "+tmp_img+" "+tmp_ppm)
-    os.system(CFG_PATH_GIFTFTSEXTRACT+" "+tmp_ppm)
+
+    """ Change image size """
+    os.system(CFG_PATH_CONVERT + " -geometry 256x256! " 
+        + tmp_img + " " + tmp_ppm)
+    """ Extract features from image """
+    os.system(CFG_PATH_GIFTFTSEXTRACT + " " + tmp_ppm)
+    """ If feature description is not written yet,
+        write it now """   
     if(not os.path.isfile(tmp_desc)):
-        os.system(CFG_PATH_GIFTWRITEFTSDESC+" <"+tmp_ppm+" >"+tmp_desc)
+        os.system(CFG_PATH_GIFTWRITEFTSDESC + " <" + tmp_ppm + " >" + tmp_desc)
         column_filter(tmp_desc,CFG_GIFTINDEX_DESC,' ',[0,1])
+    """ Move the feature file into gift index folder """
     featurefilename = CFG_GIFTINDEX_PREFIX+"/"+filename+".fts"
+    """ Delete all temporary files """
     os.rename(tmp_fts, featurefilename)
     os.remove(tmp_img)
     os.remove(tmp_ppm)
     return featurefilename
 
 def generate_gift_config_file(num_img):
+    """ This function generate gift config file.
+        Only one collection is set with name of "invenio"
+    """
     configET = ET.ElementTree()
     configET.parse(CFG_PATH_GIFTCONFIG_TEMPLATE)
 
@@ -228,7 +329,8 @@ def generate_gift_config_file(num_img):
     collectionxml.set("cui-base-dir","%s" %(CFG_GIFTINDEX_PREFIX+'/'))
     collectionxml.set("cui-inverted-file-location","InvertedFile.db")
     collectionxml.set("cui-offset-file-location","InvertedFileOffset.db")
-    collectionxml.set("cui-feature-description-location","InvertedFileFeatureDescription.db")
+    collectionxml.set("cui-feature-description-location",
+                      "InvertedFileFeatureDescription.db")
     collectionxml.set("cui-feature-file-location","url2fts.xml")
 
     queryparadigmlist = ET.SubElement(collectionxml,"query-paradigm-list")
@@ -239,17 +341,6 @@ def generate_gift_config_file(num_img):
 
     configET.write(CFG_PATH_GIFTCONFIG, "UTF-8")
     replace_in_files(CFG_PATH_GIFTCONFIG, r'''__COLLECTION__''', r'''invenio''')
-
-def gift_read_url2fts():
-    url2ftsTable = {}
-    url2ftsET = ET.ElementTree()
-    url2ftsET.parse(CFG_PATH_URL2FTS)
-    xmlnodes = url2ftsET.findall("//image")
-    for xmlnode in xmlnodes:
-        urlpostfix = xmlnode.attrib["url-postfix"]
-        recpostfix = xmlnode.attrib["thumbnail-url-postfix"]
-        url2ftsTable[urlpost]=recpostfix
-    return url2ftsTable
 
 def get_bibrankmethod_lastupdate(rank_method_code):
     """return the last excution date of bibrank method
@@ -262,8 +353,9 @@ def get_bibrankmethod_lastupdate(rank_method_code):
     return r
 
 def get_last_modified_rec(bibrank_method_lastupdate):
-    """ return the list of recods which have been modified after the last execution
-        of bibrank method. The result is expected to have ascending numerical order.
+    """ return the list of recods which have been modified after the 
+        last execution of bibrank method. The result is expected to 
+        have ascending numerical order.
     """
     query = """SELECT id FROM bibrec
                WHERE modification_date >= '%s' """ % bibrank_method_lastupdate
@@ -272,26 +364,31 @@ def get_last_modified_rec(bibrank_method_lastupdate):
     return list
 
 def replace_in_files(filepath, searchregx, replacestring):
-    cregex=re.compile(searchregx)
+    """Read a file, replace regular expression by given string."""
+    # cregex = re.compile(searchregx)
     input = open(filepath,'rb')
-    s=unicode(input.read(),'utf-8')
+    s = unicode(input.read(),'utf-8')
     input.close()
-    outtext = re.sub(searchregx,replacestring, s)
+    outtext = re.sub(searchregx, replacestring, s)
     output = open(filepath,'w')
     output.write(outtext)
     output.close()
 
 def column_filter(filename, fileoutput, separator, selected_columns):
+    """ Read a csv file, write the selected column into the output file."""
     output = csv.writer(open(fileoutput, 'w'), delimiter=separator)
-    for line in csv.reader( open(filename), delimiter=separator, skipinitialspace=True):
+    inputs = csv.reader(open(filename), delimiter=separator, skipinitialspace=True)
+    for line in inputs:
         if line: # Make sure there's at least one entry.
-            list=[]
+            list = []
             for ind in selected_columns:
                 list.append(line[ind])
             output.writerow(list)
 
 def cfgGetParameters(rank_method_code):
-    """get parameters from cfg file, the cfg file is named by rank_method_code.cfg"""
+    """ get parameters from cfg file, 
+        the cfg file is named by rank_method_code.cfg
+    """
     write_message("Running rank method: %s" % rank_method_code, verbose=0)
     try:
         file_ = CFG_ETCDIR + "/bibrank/" + rank_method_code + ".cfg"
--- modules/bibrank/lib/bibrank_gift_searcher.py
+++ modules/bibrank/lib/bibrank_gift_searcher.py
@@ -19,6 +19,7 @@
 
 __revision__ = "$Id$"
 
+import xml.etree.ElementTree as ET
 from invenio.config import CFG_PREFIX, CFG_PYLIBDIR
 from invenio.shellutils import *
 
@@ -26,14 +27,35 @@ from invenio.shellutils import *
 CFG_GIFTINDEX_PREFIX = CFG_PREFIX + "/var/gift-index-data"
 CFG_PATH_URL2FTS = CFG_GIFTINDEX_PREFIX+"/url2fts.xml"
 CFG_LIBDIR = CFG_PYLIBDIR + "/../"
+# The perl executable is used to run the query with GIFT perl API
+CFG_PERL_QUERY_SCRIPT = CFG_LIBDIR+"perl/invenio/gift_query_by_imgurl.pl"
 
 def get_similar_visual_recids(imgurls):
+    """ This function is used to perform the query for image retrieval
+        In parameter imgurls a list of image urls are passed
+        Those started with '+' are similar images
+        Those with '-' are disimilar images
+        This function just use '&' to join them into one string
+        It is in perl script CFG_PERL_QUERY_SCRIPT the query is executed
+    """
     imgurl =  '&'.join(imgurls)
     # executable_line = "/opt/cds-invenio/lib/perl/gift_query_by_imgurl.pl " + imgurl
-    error_code, output, erroroutput = run_shell_command(CFG_LIBDIR+"perl/invenio/gift_query_by_imgurl.pl %s %s", (imgurl, CFG_PATH_URL2FTS,))
-    # output = subprocess.Popen([CFG_LIBDIR+"perl/invenio/gift_query_by_imgurl.pl", imgurl, CFG_PATH_URL2FTS], stdout = subprocess.PIPE)
-    # lines = string.split(output.communicate()[0], '\n')
-    # raise repr(lines)
+    error_code, output, error_output = run_shell_command(
+        "CFG_PERL_QUERY_SCRIPT %s %s", (imgurl, CFG_PATH_URL2FTS,))
+    if (error_code != 0):
+        write_message("Image urls are found at : %s" % 
+            (icon_obj+"_"+icon_url_tag), stream=sys.sterr, verbose=0)
+
+    """ First 100 similar records are returned by pairs.
+        It is in this form :
+           recid1  similarityValue1\n
+           recid2  similarityValue2\n 
+           ......
+        The number of records returned is not parameterable.
+        But it can be done by editing the perl script directly.
+        In general 100 is enough as the max number of records 
+        in the graphical interface is 100
+    """
     lines = str.split(output, '\n')
     lines.reverse()
     results = {}
@@ -43,11 +65,25 @@ def get_similar_visual_recids(imgurls):
     for line in lines:
         if (line):
             ans = str.split(line)
-            # results_gift_recIDs.append(int(ans[0]))
-            # results_gift_rels.append(float(ans[1]))
             results[0] = results_gift_recIDs.append(int(ans[0]))
             results[1] = results_gift_rels.append(float(ans[1]))
 
     results[0] = results_gift_recIDs
     results[1] = results_gift_rels
     return results
+
+def gift_read_url2fts():
+    """ read url2fts.xml file, which is the mapping 
+        file of feature file to image url and recid.
+        One can check whether a certain image has 
+        extracted features by searching in this list. 
+    """
+    url2ftsTable = {}
+    url2ftsET = ET.ElementTree()
+    url2ftsET.parse(CFG_PATH_URL2FTS)
+    xmlnodes = url2ftsET.findall("//image")
+    for xmlnode in xmlnodes:
+        urlpostfix = xmlnode.attrib["url-postfix"]
+        recpostfix = xmlnode.attrib["thumbnail-url-postfix"]
+        url2ftsTable[urlpostfix] = recpostfix
+    return url2ftsTable
